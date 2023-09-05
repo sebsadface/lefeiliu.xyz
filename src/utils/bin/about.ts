1dc8aceb8c33b6cc429dde08fe86de74100f9f5e
Sebastian Liu <sebsadface@gmail.com>
2023-09-05 11:45:03 -0700

    0.2.1 minor changes to bannner

--- src/utils/bin/about.ts
+++ src/utils/bin/about.ts
@@ -2,5 +2,5 @@ import { getBio } from '../../api';
 
 export const about = async (args: string[]): Promise<string> => {
   const bio = await getBio();
-  return bio + '\n';
+  return bio + '\n\n';
 };
